4e9e276fc106cebdcb54ab727ebe45bb0795761a
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-02-01 09:34:22 -0700

    A2 added

--- basicServers/helloWorldArg.js
+++ basicServers/helloWorldArg.js
@@ -1,8 +1,7 @@
 var http = require("http");
 
 var callback = function (req, res) { // req -> request object; res -> response object
-  var eq = req.url.replace("/","");;
-  console.log(eq);
+  var eq = req.url.replace("/","");
   var r = "Hello World " + eq;
    res.writeHead(200, {'Content-Type': 'text/plain'}); // send response header
    res.end(r); // send response body
