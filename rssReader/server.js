a4ea26e86f0c16fc7dc2a70af81d5f37d3da3f7b
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-02-15 11:46:26 -0700

    ac

---
+++ rssReader/server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+var rssList = [];
+
+app.get("/", function (req, res) {
+      res.redirect("/index.html");
+});
+
+
+app.get("/getFeeds", function (req, res) {
+    res.send(JSON.stringify(rssList));
+});
+
+app.get("/addFeed", function (req, res) {
+  var feed = req.query.feed;
+  rssList.push(feed);
+  res.send(JSON.stringify(rssList));
+});
+
+
+
+
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
