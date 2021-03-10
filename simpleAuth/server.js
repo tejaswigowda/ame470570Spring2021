fe4cdfe0942615ea18f2cde18b01d6857c941488
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-03-10 11:01:14 -0700

    ac

--- simpleAuth/server.js
+++ simpleAuth/server.js
@@ -2,7 +2,7 @@ var url = require("url"),
 	querystring = require("querystring");
 var passport = require('passport');
 var fs = require('fs');
-var dbURL = 'mongodb://127.0.0.1:27017/test';
+	var dbURL = 'mongodb://34.219.163.158:27017/test';
 var path = require('path'),
   express = require('express'),
   db = require('mongoskin').db(dbURL);
@@ -24,7 +24,7 @@ app.use(session( {store: new MongoStore({
 app.use(passport.initialize());
 app.use(passport.session());
 var flash = require('express-flash');
-app.use( flash() );      
+app.use( flash() );
 
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
@@ -32,7 +32,7 @@ var methodOverride = require("method-override");
 app.use(methodOverride());
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended:false 
+  extended:false
 }));
 require('./passport/config/passport')(passport); // pass passport for configuration
 require('./passport/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
