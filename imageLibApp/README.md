8180103366172a5bb8d98c4963c3752d261ae29c
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-04-07 10:57:32 -0700

    ac

---
+++ imageLibApp/README.md
@@ -0,0 +1,9 @@
+# Simple authentication using Node.js + Passport.js
+
+## Usage
+
+0. Make sure mongoDB is running at `mongodb:127.0.0.1:27017`.
+1. clone repo: `git clone https://github.com/tejaswigowda/simpleAuth.git`.
+2. `cd simpleAuth`
+3. `node server.js`
+4. Goto http://localhost:8080
---
+++ imageLibApp/https.js
@@ -0,0 +1,18 @@
+var homeDir = require('path').join(require('os').homedir());
+require('greenlock-express').create({
+  version: 'draft-11'
+, server: 'https://acme-v02.api.letsencrypt.org/directory'
+//, server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+, email: ''                                     // CHANGE THIS
+, agreeTos: true
+, approveDomains: [ 'example.com', 'www.example.com' ]              // CHANGE THIS
+, store: require('greenlock-store-fs')
+, configDir: homeDir
+//, app: require('express')().use('/', function (req, res) {
+//    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+//    res.end('Hello, World!\n\n💚 🔒.js');
+//  })
+, app: require('./server.js')
+//, communityMember: true
+}).listen(8080, 8443);
+
